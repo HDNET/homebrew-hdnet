dc36d5d8e9d0a04f4e3967e69c5d836452a77572
Jeffrey Boehm <jeffrey.boehm@hdnet.de>
2014-09-11 16:20:23 +0200

    bash_completion patchen

--- Formula/magerun.rb
+++ Formula/magerun.rb
@@ -7,6 +7,11 @@ class Magerun < Formula
 
   depends_on "bash-completion"
 
+  patch do
+    url "https://gist.githubusercontent.com/jeboehm/2916d673260a5f6e6b5f/raw/46db8c5565dd7d13b30042018206fd2fd800f705/bash_complete.patch"
+    sha1 "622d3ac24e24f160399c56d2107563ada3fe1efb"
+  end
+
   def install
     (bin).install "n98-magerun.phar" => "magerun"
     (bash_completion).install "autocompletion/bash/bash_complete" => "n98-magerun.bash"
