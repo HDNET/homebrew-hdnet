fdc6d1a9515a0be5ce83f1a553eac540a612afd8
Jeffrey Boehm <jeffrey.boehm@hdnet.de>
2015-03-29 17:01:07 +0200

    Bugfix 2

--- Formula/symfony-installer.rb
+++ Formula/symfony-installer.rb
@@ -1,10 +1,10 @@
-require "formula"
-
 class SymfonyInstaller < Formula
-  homepage 'http://symfony.com/'
-  url 'https://github.com/symfony/symfony-installer'
+  homepage "http://symfony.com/"
+  url "http://get.sensiolabs.org/symfony.phar"
+  version "1"
+  sha256 "d209995a19452c116870d88a94a71dede8c541ad27402cab5c7ff01bc8a5d4de"
 
   def install
-    (bin).install "symfony" => "symfony"
+    (bin).install "symfony.phar" => "symfony"
   end
 end
