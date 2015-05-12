0c8b407f10e51c1fd9d4bcea5ce1a49e4deba50a
Marc Paffen <marc.paffen@hdnet.de>
2015-05-12 15:14:06 +0200

    install git template

--- Formula/hdnet-coding-standards.rb
+++ Formula/hdnet-coding-standards.rb
@@ -1,21 +1,23 @@
 class HdnetCodingStandards < Formula
   homepage "https://www.hdnet.de"
-  url "https://github.com/HDNET/coding-standards/archive/0.1.tar.gz"
-  sha256 "6d5945bd21e1a7d5f6bc523a1a0d1a16fcf2aa89c64b130e54cc5361a76dc8bd"
+  url "https://github.com/HDNET/coding-standards/archive/0.1.1.tar.gz"
+  sha256 "e9f49b2be1b53ddbca63417200b948885b865283957d4c3cd8ba5b7d92c78570"
 
   depends_on "symfony2-coding-standard"
 
   keg_only "Wir brauchen nur eine Verlinkung in etc"
 
   def caveats
-  	"
+  "
 Für Team-Black:
 phpcs --config-set default_standard HDNET-Black
-  	"
+git config --global template "+prefix+"/git-templates/template-black
+  "
   end
 
   def install
     prefix.install "HDNET-Black"
+    prefix.install "git-templates"
     (etc+"php-code-sniffer"+"Standards").install_symlink prefix+"HDNET-Black"
   end
 end
