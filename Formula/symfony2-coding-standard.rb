16d8304f16631e88b895c3a3bf7adee0cc19a43e
sofakartoffel <marc.paffen@googlemail.com>
2017-06-22 23:14:44 +0200

    update symfony2-coding-standard, depends on php-code-sniffer 2.9

--- Formula/symfony2-coding-standard.rb
+++ Formula/symfony2-coding-standard.rb
@@ -1,14 +1,16 @@
 class Symfony2CodingStandard < Formula
   homepage "https://github.com/escapestudios/Symfony2-coding-standard"
-  url "https://github.com/escapestudios/Symfony2-coding-standard/archive/2.4.0.tar.gz"
-  sha256 "4dbbce93afe3c7aca65a0b2f5ff24fb4863fd506e731155c0961ca97a708b9fb"
+  url "https://github.com/escapestudios/Symfony2-coding-standard/archive/2.11.0.tar.gz"
+  sha256 "70e828d886b89c7ecdd3911379fea1b5ffdbf5fdf1a1aa12d58d854338fc2dbd"
 
-  depends_on "php-code-sniffer"
+  depends_on "php-code-sniffer@2.9"
 
   keg_only "Wir brauchen nur eine Verlinkung in etc"
 
   def install
     prefix.install "Symfony2"
+
+    FileUtils.rm (etc+"php-code-sniffer"+"Standards")+"Symfony2", :force => true
     (etc+"php-code-sniffer"+"Standards").install_symlink prefix+"Symfony2"
   end
 end
